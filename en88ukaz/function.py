3ceacea262a1eedb964676649848c2e0019eff52
hamideh-hamidnezhad <hamideh.hamidnezhad@fau.de>
2021-11-15 00:15:29 +0100

    debug

--- en88ukaz/function.py
+++ en88ukaz/function.py
@@ -1,7 +1,6 @@
 import numpy as np
-from ipywidgets
-import interact, fixed from PIL
-import Image 
+from ipywidgets import interact, fixed 
+from PIL import Image 
 
 def imshow(X , resize = None): 
     """
